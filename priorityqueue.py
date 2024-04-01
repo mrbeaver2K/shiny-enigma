4ae2d6fcdcd7f8c9025d93a38364d9993b4490ec
mrbeaver2K <112230379+mrbeaver2K@users.noreply.github.com>
2024-03-31 21:33:18 -0700

    Added example to priorityqueue.py

--- priorityqueue.py
+++ priorityqueue.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env/python3
 class pq():
 	def __init__(self):
 		self.queue = [None]
 	def add(self, item):
+                """Adds an item to the appropriate place in the queue"""
 		self.queue.append(item)
 		pointer = self.getLen()
 		while pointer > 1:
@@ -10,6 +12,7 @@ class pq():
 			else:
 				break
 	def pull(self):
+                """Removes and returns the largest item in the queue"""
 		result = self.queue[1]
 		self.queue[1] = self.queue.pop()
 		pointer = 1
@@ -24,4 +27,16 @@ class pq():
 				break
 		return result
 	def getLen(self):
+                """Returns the adjusted length of the queue"""
 		return len(self.queue) - 1
+
+test = pq()
+print("Input: (10, 20, 30, 40, 50)")
+for i in (10, 20, 30, 40, 50):
+	test.add(i)
+print("Sorted:")
+print(test.pull())
+print(test.pull())
+print(test.pull())
+print(test.pull())
+print(test.pull())
