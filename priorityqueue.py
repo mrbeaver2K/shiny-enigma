b4e2abb5830c6998c892f66c884807c5f77bc7dc
mrbeaver2K <derekdelatorre@icloud.com>
2024-04-24 16:05:11 -0700

    Your branch is up to date with 'origin/master'.

--- priorityqueue.py
+++ priorityqueue.py
@@ -3,40 +3,48 @@ class pq():
 	def __init__(self):
 		self.queue = [None]
 	def add(self, item):
-                """Adds an item to the appropriate place in the queue"""
+		"""Adds an item to the appropriate place in the queue"""
 		self.queue.append(item)
 		pointer = self.getLen()
 		while pointer > 1:
 			if self.queue[pointer] > self.queue[pointer // 2]:
 				self.queue[pointer], self.queue[pointer // 2] = self.queue[pointer // 2], self.queue[pointer]
+				pointer = pointer // 2
 			else:
 				break
 	def pull(self):
-                """Removes and returns the largest item in the queue"""
+		"""Removes and returns the largest item in the queue"""
 		result = self.queue[1]
-		self.queue[1] = self.queue.pop()
-		pointer = 1
-		while True:
-			if pointer * 2 > self.getLen():
-				break
-			if self.queue[pointer * 2] >  self.queue[pointer] and (pointer * 2 == len(self.queue) or self.queue[pointer * 2 + 1] < self.queue[pointer]):
-				self.queue[pointer], self.queue[pointer * 2] = self.queue[pointer * 2], self.queue[pointer]
-			elif self.queue[pointer * 2 + 1] >  self.queue[pointer]:
-				self.queue[pointer], self.queue[pointer * 2] = self.queue[pointer * 2], self.queue[pointer]
-			else:
-				break
+		if self.getLen() > 1:
+			self.queue[1] = self.queue.pop()
+			pointer = 1
+			while pointer * 2 < self.getLen():
+				if self.queue[pointer * 2] >  self.queue[pointer] or (pointer * 2 == len(self.queue) or self.queue[pointer * 2 + 1] < self.queue[pointer]):
+					self.queue[pointer], self.queue[pointer * 2] = self.queue[pointer * 2], self.queue[pointer]
+					pointer = pointer * 2
+				elif self.queue[pointer * 2 + 1] >  self.queue[pointer]:
+					self.queue[pointer], self.queue[pointer * 2 + 1] = self.queue[pointer * 2 + 1], self.queue[pointer]
+					pointer = pointer * 2 + 1
+				else:
+					break
+		else:
+			return self.queue.pop()
 		return result
 	def getLen(self):
-                """Returns the adjusted length of the queue"""
+		"""Returns the adjusted length of the queue"""
 		return len(self.queue) - 1
+
+def run(s, t=1, d=True):
+	for i in range(0, t):
+		print(s, end="\t" if d else "\n")
+		if d:
+			print(eval(s))
+		else:
+			exec(s)
 
 test = pq()
 print("Input: (10, 20, 30, 40, 50)")
 for i in (10, 20, 30, 40, 50):
-	test.add(i)
+	run(f"test.add({i})", d=False)
 print("Sorted:")
-print(test.pull())
-print(test.pull())
-print(test.pull())
-print(test.pull())
-print(test.pull())
+run("test.pull()", 5)
