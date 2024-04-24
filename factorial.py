f9d5f429492868be51afb8b23fcae296a759fbdc
mrbeaver2K <112230379+mrbeaver2K@users.noreply.github.com>
2024-04-23 20:57:18 -0700

    Finished factorial.py.

--- factorial.py
+++ factorial.py
@@ -4,6 +4,12 @@ def factorial(n):
         acc *= i
     return acc
 
+def factorial_recursive(n, p=1):
+    if n - 1 > 0:
+        return factorial_recursive(n - 1, (n - 1) * p)
+    else:
+        return p
+
 def run(s):
     print(s, end="\t")
     print(eval(s))
@@ -11,3 +17,5 @@ def run(s):
 print("We are demonstrating the factorial function two different ways: as a loop and recursively.")
 for i in range(0, 16):
     run(f"factorial({i})")
+for i in range(0, 16):
+    run(f"factorial_recursive({i})")
