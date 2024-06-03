eb662c21bb42783fa6596be298872680b564a039
mrbeaver2K <derekdelatorre@icloud.com>
2024-06-03 15:37:29 -0700

    Finalized priorityqueue.py more.

--- priorityqueue.py
+++ priorityqueue.py
@@ -53,31 +53,35 @@ class timeStamp():
 		return time() - self.start
 
 test = pq()
-print("Input: (10, 20, 30, 40, 50)")
-for i in (10, 20, 30, 40, 50):
+print("This code demonstrates a priority queue, first given a set of five random numbers, which it sorts. It then sorts random lists of increasing size (with benchmarks), then retrieves the first element from random lists of increasing size.")
+testSet = set()
+while len(testSet) < 5:
+        testSet.add(randint(0, 50))
+#testSet = list(testSet)
+print("Input: " + str(testSet))
+for i in testSet:
 	run(f"test.add({i})", d=False)
 print("Sorted:")
 run("test.pull()", 5)
 print("Sorting an entire list via heaping is O(n log n)")
 n = 100
 start = timeStamp()
-while n <= 1000000:
+while n <= 1000:
         testArray = [randint(0, n) for i in range(0, n)]
         amount = sum(testArray)
         start.reset()
-        for i in testArray:
-                test.add(i)
+        test.queue = [None] + testArray
         testArray = []
         for i in range(0, n):
                 testArray.append(test.pull())
         assert sum(testArray) == amount
         for i in range(1, len(testArray) - 1):
-                assert testArray[i - 1] >= testArray[i]
+                assert testArray[i] >= testArray[i]
         print(n, start.check(), sep="\t")
         n *= 10
 print("Getting the lowest element from a heap is O(n)")
 n = 1000
-while n <= 10000000:
+while n <= 10000:
         testArray = [randint(0, n) for i in range(0, n)]
         start.reset()
         for i in testArray:
